207eb9e259022c813c4dc400d7e374c0f8951d7e
Satinatrish <trishaannsatina@gmail.com>
2024-10-11 09:45:58 +0800

    added methods

--- abstraction.js
+++ abstraction.js
@@ -2,6 +2,21 @@ class Employee {
     #name;
     #baseSalary;
 
-  
+    setName(val) {
+        this.#name = val;
+    }
+
+    setBaseSalary(val) {
+        this.#baseSalary = val;
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    getSalary() {
+        let bonus = 1000;
+        return this.#baseSalary + bonus;
+    }
 }
 
